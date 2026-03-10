e92c7d137180513c576138ede6749389e1c18594
Carlso16 <coronaspain221@gmail.com>
2026-03-10 19:20:13 +0100

    Update main.cpp

--- JRG26/src/main.cpp
+++ JRG26/src/main.cpp
@@ -1,97 +1,58 @@
 #include <Arduino.h>
-#include <Servos.h>
-
 #define pinModeMux 19
-#define unmbralON 3000
-#define volantazo 350
-
-#include <QTRSensors.h>
-const uint8_t sensorPins[] = {26,25,33,32,35,34,39,36};
-uint16_t  sensorValues[8];
-boolean perdidaDeLinea = false;
-boolean perdidaDeLineaAnt = false;
-boolean estadoSensores[8];
-boolean estadoSensoresAnt[8];
-boolean* ptr = estadoSensores;    
-boolean* ptrAnt = estadoSensoresAnt;
-QTRSensors qtr;
-
-int setPoint = 3500;
-int vBase = 25; //orden 20
-int accion = 0;
-int error = 0;
-int errorAnt = 0;
-int errorCong = 0;
-float Kp = 0.1; //orden 0.05
-float Kd = 0;
-float Ki = 0;
-int sentidoi = 0;
-int sentidod = 0;
-
-void aplicarAccion(int accion);
-void leerSensores();
+#define pinMotorChulo1 27
+#define pinMotorChulo2 14
 
 void setup() {
   // Inicializa la comunicación serie a 115200 baudios
   Serial.begin(115200);
   pinMode(pinModeMux,OUTPUT);
-  pinMode(2,OUTPUT);
-  initPWM();
+  pinMode(pinMotorChulo1,OUTPUT);
+  pinMode(pinMotorChulo2,OUTPUT);
+  digitalWrite(pinMotorChulo1,LOW);
+  digitalWrite(pinMotorChulo2,LOW);
   digitalWrite(pinModeMux,LOW);
-  qtr.setTypeAnalog();
-  qtr.setSensorPins(sensorPins,8);
-  delay(500);
-  ruedaDelDcha(0);
-    for (uint16_t i = 0; i < 200; i++)
-  {
-    qtr.calibrate();
-  }
-  ruedaDelDcha(0);
-  delay(500);
 }
 
 void loop() {
-  uint16_t position =  qtr.readLineBlack(sensorValues);
-  qtr.read(sensorValues);
-  for (uint8_t i = 0; i < 7; i++) {
-    if (sensorValues[i] > unmbralON) {
-      estadoSensores[i] = true;  // Sensor detecta la línea (ON)
-    } else {
-      estadoSensores[i] = false; // Sensor no detecta la línea (OFF)
-    }
-  }
-  perdidaDeLinea = true;
-  for(int i = 0; i < 7; i++){
-    perdidaDeLinea = perdidaDeLinea && not(estadoSensores[i]);
-  }
-  if(perdidaDeLinea && !perdidaDeLineaAnt){
-    if(estadoSensoresAnt[0] || estadoSensoresAnt[1]){
-      errorCong = volantazo;
-    }
-    else{
-      errorCong = -volantazo;
-    }
-  }
-  digitalWrite(2,perdidaDeLinea);
-  error = setPoint - position;
-  if(perdidaDeLinea)
-    error = errorCong;
-  accion = Kp*error + Kd*(error-errorAnt) + Ki*(error+errorAnt); // + Ki*(error + errorAnt) + Kd*(error - errorAnt) creo
-  aplicarAccion(accion);
-  //leerValoresK();
-  errorAnt = error;
-  perdidaDeLineaAnt = perdidaDeLinea;
-  for(int i = 0; i < 7; i++){
-    estadoSensoresAnt[i] = estadoSensores[i];
-  }
-  delay(10);
+  // Lee los valores brutos de los pines analógicos
+  int rawD1 = analogRead(36);  // D1: pin 26
+  int rawD2 = analogRead(39);  // D2: pin 25
+  int rawD3 = analogRead(34);  // D3: pin 33
+  int rawD4 = analogRead(35);  // D4: pin 32
+  int rawD5 = analogRead(32);  // D5: pin 35
+  int rawD6 = analogRead(33);  // D6: pin 34
+  int rawD7 = analogRead(25);   // D7: pin 39
+  int rawD8 = analogRead(26);   // D8: pin 36
+  
+  // Convierte el valor bruto a voltaje (0 a 3.3V) usando la resolución de 12 bits (4095)
+  float voltageD1 = rawD1 * (3.3 / 4095.0);
+  float voltageD2 = rawD2 * (3.3 / 4095.0);
+  float voltageD3 = rawD3 * (3.3 / 4095.0);
+  float voltageD4 = rawD4 * (3.3 / 4095.0);
+  float voltageD5 = rawD5 * (3.3 / 4095.0);
+  float voltageD6 = rawD6 * (3.3 / 4095.0);
+  float voltageD7 = rawD7 * (3.3 / 4095.0);
+  float voltageD8 = rawD8 * (3.3 / 4095.0);
+  
+  // Imprime los valores de tensión en una sola línea
+  Serial.print("D1: ");
+  Serial.print(voltageD1, 2);
+  Serial.print(" V\tD2: ");
+  Serial.print(voltageD2, 2);
+  Serial.print(" V\tD3: ");
+  Serial.print(voltageD3, 2);
+  Serial.print(" V\tD4: ");
+  Serial.print(voltageD4, 2);
+  Serial.print(" V\tD5: ");
+  Serial.print(voltageD5, 2);
+  Serial.print(" V\tD6: ");
+  Serial.print(voltageD6, 2);
+  Serial.print(" V\tD7: ");
+  Serial.print(voltageD7, 2);
+  Serial.print(" V\tD8: ");
+  Serial.println(voltageD8, 2);
+  
+  // Espera 1 segundo antes de la siguiente lectura
+  delay(1000);
 }
-void aplicarAccion(int accion){
-  int velIzda = constrain(vBase + accion, -100, 100);
-  int velDcha = constrain(vBase - accion, -100, 100);
-  ruedasIzda(velIzda);
-  ruedaTrasDcha(velDcha); 
-  ruedaDelDcha(velDcha);
-}
-
-
