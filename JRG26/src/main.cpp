b03d79ac151f04fa1dd7837f352875e3be49119a
Carlso16 <coronaspain221@gmail.com>
2026-03-11 23:01:31 +0100

    Update main.cpp

--- JRG26/src/main.cpp
+++ JRG26/src/main.cpp
@@ -2,7 +2,7 @@
 #include <Servos.h>
 
 #define pinModeMux 19
-#define unmbralON 2400
+#define unmbralON 1900
 
 #include <QTRSensors.h>
 const uint8_t sensorPins[] = {26,25,33,32,35,34,39,36};
@@ -21,8 +21,8 @@ int accion = 0;
 int error = 0;
 int errorAnt = 0;
 int errorCong = 0;
-float Kp = 0.05; //orden 0.05
-float volantazo = 75/Kp;
+float Kp = 0.075; //orden 0.05
+float volantazo = 85/Kp;
 float Kd = 0;
 float Ki = 0;
 int sentidoi = 0;
